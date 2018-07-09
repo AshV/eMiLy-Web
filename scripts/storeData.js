89047b1d8767be469bb1a7cdd4872291385d99db
AshV <ashish6318@gmail.com>
2018-07-09 06:06:59 +0530

    completed

--- scripts/storeData.js
+++ scripts/storeData.js
@@ -11,7 +11,8 @@ function newFileId() {
 
 var fileId = "";
 
-function makeFileEntry(fileName, column) {
+function makeFileEntry(file, fileName, column) {
+    debugger;
     fileId = newFileId();
     makeLocalEntry(fileId);
     firebase.database().ref('files/' + fileId).set({
@@ -22,7 +23,7 @@ function makeFileEntry(fileName, column) {
         localFilePath: "TBS"
     });
     debugger;
-    triggerTheJob("PCFET0NUWVBFIGh0bWw+DQo8aHRtbD4NCjxoZWFkPg0KICAgIDx0aXRsZT5lTWlMeSB8IFJlc3VsdHM8L3RpdGxlPg0KICAgIDxzY3JpcHQgc3JjPSJodHRwczovL3d3dy5nc3RhdGljLmNvbS9maXJlYmFzZWpzLzUuMS4wL2ZpcmViYXNlLWFwcC5qcyI+PC9zY3JpcHQ+DQogICAgPHNjcmlwdCBzcmM9Imh0dHBzOi8vd3d3LmdzdGF0aWMuY29tL2ZpcmViYXNlanMvNS4xLjAvZmlyZWJhc2UtZGF0YWJhc2UuanMiPjwvc2NyaXB0Pg0KICAgIDxzY3JpcHQgc3JjPSJodHRwczovL3d3dy5nc3RhdGljLmNvbS9maXJlYmFzZWpzLzUuMS4wL2ZpcmViYXNlLmpzIj48L3NjcmlwdD4NCiAgICA8c2NyaXB0Pg0KICAgICAgICAvLyBJbml0aWFsaXplIEZpcmViYXNlDQogICAgICAgIC8vIFRPRE86IFJlcGxhY2Ugd2l0aCB5b3VyIHByb2plY3QncyBjdXN0b21pemVkIGNvZGUgc25pcHBldA0KICAgICAgICB2YXIgY29uZmlnID0gew0KICAgICAgICAgICAgYXBpS2V5OiAiPEFQSV9LRVk+IiwNCiAgICAgICAgICAgIGF1dGhEb21haW46ICI8UFJPSkVDVF9JRD4uZmlyZWJhc2VhcHAuY29tIiwNCiAgICAgICAgICAgIGRhdGFiYXNlVVJMOiAiaHR0cHM6Ly9vdXRsYXlzdG9yZS5maXJlYmFzZWlvLmNvbSIsDQogICAgICAgICAgICBwcm9qZWN0SWQ6ICI8UFJPSkVDVF9JRD4iLA0KICAgICAgICAgICAgc3RvcmFnZUJ1Y2tldDogIjxCVUNLRVQ+LmFwcHNwb3QuY29tIiwNCiAgICAgICAgICAgIG1lc3NhZ2luZ1NlbmRlcklkOiAiPFNFTkRFUl9JRD4iLA0KICAgICAgICB9Ow0KICAgICAgICBmaXJlYmFzZS5pbml0aWFsaXplQXBwKGNvbmZpZyk7DQogICAgPC9zY3JpcHQ+DQogICAgPHNjcmlwdCBzcmM9InNjcmlwdHNcc3RvcmVEYXRhLmpzIj48L3NjcmlwdD4NCg0KPC9oZWFkPg0KPGJvZHk+DQoNCjwvYm9keT4NCjwvaHRtbD4=", fileName, column, fileId);
+    triggerTheJob(getBase64(file), fileName, column, fileId);
 }
 
 function makeLocalEntry(fileId) {
@@ -42,14 +43,25 @@ function triggerTheJob(file_stream, file_name, column_name, file_id) {
     fetch(
         "http://localhost:8080/postjob",
         {
-            body: '{ "file_stream": "' + file_stream + '", "file_name": "' + file_name + '", "column_name": "' + column_name + '", "file_id": "' + file_id+'" }',
+            body: '{ "file_stream": "' + file_stream + '", "file_name": "' + file_name + '", "column_name": "' + column_name + '", "file_id": "' + file_id + '" }',
             headers: {
                 "Content-Type": "application/json"
             },
-           // credentials: "same-origin",
+            // credentials: "same-origin",
             method: "POST"
         })
         .then(response => response.json())
         .then(data => alert(data))
         .catch(error => console.error("Error:", error));
+}
+
+function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
 }
